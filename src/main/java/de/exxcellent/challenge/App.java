f426966e27e9ea76d38d422bdc394c092a457ec6
Kubilay Anil <kubilay.anil@rsu-reifen.de>
2021-01-22 20:27:48 +0100

    output lowest spread day in main

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,7 @@
 package de.exxcellent.challenge;
 
+import task.weather.WeatherTask;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -14,10 +16,10 @@ public final class App {
      */
     public static void main(String... args) {
 
-        // Your preparation code …
+        WeatherTask weatherTask = new WeatherTask();
 
         String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+        System.out.printf("Day with smallest temperature spread : %s%n", weatherTask.getDayWithSmallestTempSpread());
 
         String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
         System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
